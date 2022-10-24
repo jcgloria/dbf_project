792d115f4d53ddcd0840e6e7576b59df278cb722
jcgloria <96juancg@gmail.com>
2022-10-24 13:36:50 +0100

    added categories to mock data script

--- python_mock_data/script.py
+++ python_mock_data/script.py
@@ -18,5 +18,14 @@ for line in file:
     data_user = (values[0], True if values[1] == 'true' else False, values[2], values[3])
     cursor.execute(add_user, data_user)
 
+#Create categories
+file = open('categories.csv', 'r').read()
+categories = file.split(',')
+add_category = ("INSERT INTO Categories "
+               "(category) "
+               "VALUES (%s)")
+for category in categories:
+    cursor.execute(add_category, (category,))
+
 cnx.commit()
 cnx.close()
