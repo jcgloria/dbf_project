974fd87d55efe47d18e24cba3c91d54a5153f86f
jcgloria <96juancg@gmail.com>
2022-10-24 12:25:12 +0100

    users mockdata

---
+++ python_mock_data/script.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+#Connection to Mysql. Make sure port is correct. 
+cnx = mysql.connector.connect(user='root', password='root',
+                              host='localhost',
+                              port=8889,
+                              database='dbf_project')
+
+cursor = cnx.cursor()                    
+
+#Create users
+file = open('users.csv', 'r')
+for line in file:
+    values = line.split(',')
+    add_user = ("INSERT INTO Users "
+               "(username, seller, pass, email) "
+               "VALUES (%s, %s, %s, %s)")
+    data_user = (values[0], True if values[1] == 'true' else False, values[2], values[3])
+    cursor.execute(add_user, data_user)
+
+cnx.commit()
+cnx.close()
