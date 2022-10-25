7f95e85397b98ca19f0ebeb377b56e9c60622d37
xjffff <62401093+xjffff@users.noreply.github.com>
2022-10-25 01:08:05 +0100

    connected to database

--- auction/process_registration.php
+++ auction/process_registration.php
@@ -1,7 +1,68 @@
 <?php
-
+include_once('register.php');
 // TODO: Extract $_POST variables, check they're OK, and attempt to create
 // an account. Notify user of success/failure and redirect/give navigation 
 // options.
+
+//initialize varaibles
+$accountType = $name = $email = $pass = '';
+$nameErr = $emailErr = $passErr = $repassErr = '';
+
+
+//
+if(isset($_POST['register'])) {
+    //validate & sanitize name
+    if(empty($_POST['username'])) {
+        die('username is required');
+    } else {
+        $name = filter_input(INPUT_POST,'username',FILTER_SANITIZE_SPECIAL_CHARS);
+        
+    }
+    //validate & sanitize email
+    if(empty($_POST['email'])) {
+        die('Email is required');
+    } else {
+        $email = filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL);
+    }
+    //validate & sanitize password
+    if(empty($_POST['password'])) {
+        die('Email is required');
+    } else {
+        $pass = filter_input(INPUT_POST,'password',FILTER_SANITIZE_EMAIL);
+    }
+    
+    //check repeat password
+    $pass = $_POST['password'];
+    $pass_md5 = md5('DBF'.$pass);
+    if($pass != $_POST['passwordConfirmation']) {
+        die('password must be the same');
+    }
+
+    //validate whether there exist a same username in database
+    $sqll = "select*from users where username = '$name'";
+    $res = mysqli_query($conn, $sqll);
+    $sql_fetch = mysqli_fetch_row($res);
+    if($sql_fetch) {
+        die('username already exist');
+    }
+
+
+
+    if($_POST['accountType']='seller') {
+        $sql = "INSERT INTO Users(username,seller,pass,email) VALUES ('$name', 1 , '$pass_md5', '$email')";
+    } else {
+        $sql = "INSERT INTO Users(username,seller,pass,email) VALUES ('$name', 0 , '$pass_md5', '$email')";
+    }
+    if(mysqli_query($conn,$sql)) {
+        //success
+        echo "<script type='text/javascript'>alert('account sucessfully created');</script>";
+        header("Location: index.php");
+        die();
+    }  else {
+        echo 'Error: ' .mysqli_error($conn);
+        echo"<a href='register.php'>Unsucessfull</a>";
+    }   
+}
+
 
 ?>
--- auction/register.php
+++ auction/register.php
@@ -20,33 +20,40 @@
 	</div>
   </div>
   <div class="form-group row">
+    <label for="username" class="col-sm-2 col-form-label text-right">Username</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="username" id="username" placeholder="Username">
+      <small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+	</div>
+  </div>
+  <div class="form-group row">
     <label for="email" class="col-sm-2 col-form-label text-right">Email</label>
 	<div class="col-sm-10">
-      <input type="text" class="form-control" id="email" placeholder="Email">
+      <input type="text" class="form-control" name="email" id="email" placeholder="Email">
       <small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
 	</div>
   </div>
   <div class="form-group row">
     <label for="password" class="col-sm-2 col-form-label text-right">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="password" placeholder="Password">
+      <input type="password" class="form-control" name="password" id="password" placeholder="Password">
       <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
     </div>
   </div>
   <div class="form-group row">
     <label for="passwordConfirmation" class="col-sm-2 col-form-label text-right">Repeat password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="passwordConfirmation" placeholder="Enter password again">
+      <input type="password" class="form-control" name="passwordConfirmation" id="passwordConfirmation" placeholder="Enter password again">
       <small id="passwordConfirmationHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
     </div>
   </div>
   <div class="form-group row">
-    <button type="submit" class="btn btn-primary form-control">Register</button>
+    <button type="submit" class="btn btn-primary form-control" name='register'>Register</button>
   </div>
 </form>
 
 <div class="text-center">Already have an account? <a href="" data-toggle="modal" data-target="#loginModal">Login</a>
 
 </div>
-
+ 
 <?php include_once("footer.php")?>
