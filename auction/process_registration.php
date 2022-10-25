a1eee347583ad367ec14073a512bba16384b828c
xjffff <62401093+xjffff@users.noreply.github.com>
2022-10-25 11:40:53 +0100

    add nice css error notification

--- auction/process_registration.php
+++ auction/process_registration.php
@@ -1,5 +1,6 @@
 <?php
-include_once('register.php');
+//ob_start();
+include'register.php';
 // TODO: Extract $_POST variables, check they're OK, and attempt to create
 // an account. Notify user of success/failure and redirect/give navigation 
 // options.
@@ -13,20 +14,23 @@ $nameErr = $emailErr = $passErr = $repassErr = '';
 if(isset($_POST['register'])) {
     //validate & sanitize name
     if(empty($_POST['username'])) {
-        die('username is required');
+        echo '<div class="alert alert-danger"role="alert">username is required;</div>';
+        die();
     } else {
         $name = filter_input(INPUT_POST,'username',FILTER_SANITIZE_SPECIAL_CHARS);
         
     }
     //validate & sanitize email
     if(empty($_POST['email'])) {
-        die('Email is required');
+        echo '<div class="alert alert-danger"role="alert">Email is required;</div>';
+        die();
     } else {
         $email = filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL);
     }
     //validate & sanitize password
     if(empty($_POST['password'])) {
-        die('Email is required');
+        echo '<div class="alert alert-danger"role="alert">Password is required;</div>';
+        die();
     } else {
         $pass = filter_input(INPUT_POST,'password',FILTER_SANITIZE_EMAIL);
     }
@@ -35,7 +39,8 @@ if(isset($_POST['register'])) {
     $pass = $_POST['password'];
     $pass_md5 = md5('DBF'.$pass);
     if($pass != $_POST['passwordConfirmation']) {
-        die('password must be the same');
+        echo '<div class="alert alert-danger"role="alert">Password must be the same;</div>';
+        die();
     }
 
     //validate whether there exist a same username in database
@@ -43,20 +48,21 @@ if(isset($_POST['register'])) {
     $res = mysqli_query($conn, $sqll);
     $sql_fetch = mysqli_fetch_row($res);
     if($sql_fetch) {
-        die('username already exist');
+        echo '<div class="alert alert-danger"role="alert">Username already exists;</div>';
+        die();
     }
 
 
 
-    if($_POST['accountType']='seller') {
+    if($_POST['accountType']=='seller') {
         $sql = "INSERT INTO Users(username,seller,pass,email) VALUES ('$name', 1 , '$pass_md5', '$email')";
     } else {
         $sql = "INSERT INTO Users(username,seller,pass,email) VALUES ('$name', 0 , '$pass_md5', '$email')";
     }
     if(mysqli_query($conn,$sql)) {
         //success
-        echo "<script type='text/javascript'>alert('account sucessfully created');</script>";
-        header("Location: index.php");
+        echo '<div class="alert alert-success"role="alert">Account sucessfully created <a href="index.php" class="alert-link">Return to home page</a></div>';
+        "Location: index.php";
         die();
     }  else {
         echo 'Error: ' .mysqli_error($conn);
