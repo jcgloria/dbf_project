764edc11882b6184fa0efe046a2c8c7c3771f971
jay Xiao <1455482079@qq.com>
2022-11-02 15:55:35 +0000

    fix linking page

--- auction/mylistings.php
+++ auction/mylistings.php
@@ -81,7 +81,7 @@
         if ($curr_page != 1) {
           echo ('
     <li class="page-item">
-      <a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page - 1) . '" aria-label="Previous">
+      <a class="page-link" href="mylistings.php?' . $querystring . 'page=' . ($curr_page - 1) . '" aria-label="Previous">
         <span aria-hidden="true"><i class="fa fa-arrow-left"></i></span>
         <span class="sr-only">Previous</span>
       </a>
@@ -101,14 +101,14 @@
 
           // Do this in any case
           echo ('
-      <a class="page-link" href="browse.php?' . $querystring . 'page=' . $i . '">' . $i . '</a>
+      <a class="page-link" href="mylistings.php?' . $querystring . 'page=' . $i . '">' . $i . '</a>
     </li>');
         }
 
         if ($curr_page != $max_page) {
           echo ('
     <li class="page-item">
-      <a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page + 1) . '" aria-label="Next">
+      <a class="page-link" href="mylistings.php?' . $querystring . 'page=' . ($curr_page + 1) . '" aria-label="Next">
         <span aria-hidden="true"><i class="fa fa-arrow-right"></i></span>
         <span class="sr-only">Next</span>
       </a>
