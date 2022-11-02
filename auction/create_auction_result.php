35ba28b51ff4c75d7b28a79f70ae782899e5cbe5
jay Xiao <1455482079@qq.com>
2022-11-02 17:33:56 +0000

    add link to go to new created auction

--- auction/create_auction_result.php
+++ auction/create_auction_result.php
@@ -4,7 +4,7 @@
 <div class="container my-5">
 
 <?php
-
+ob_start();
 // This function takes the form data and adds the new auction to the database.
 
 /* TODO #1: Connect to MySQL database (perhaps by requiring a file that
@@ -18,17 +18,49 @@
             issue, give some semi-helpful feedback to user. */
 if(isset($_POST['createAuction'])) {
     $username = $_SESSION['username'];
-    $title = $_POST['title'];
+    //check title
+    if(empty($_POST['title'])) {
+        echo '<div class="alert alert-danger"role="alert">Title is required;</div>';
+        die();
+    } else {
+        $title = $_POST['title'];
+    }
+    
+    //don't need to check details
     $details = $_POST['details'];
-    $category = $_POST['category'];
-    $startingPrice = $_POST['startingPrice'];
+    
+    //check category
+    if(empty($_POST['category'])) {
+        echo '<div class="alert alert-danger"role="alert">Category is required;</div>';
+        die();
+    } else {
+        $category = $_POST['category'];
+    }
+
+    //check starting price
+    if(empty($_POST['startingPrice'])) {
+        echo '<div class="alert alert-danger"role="alert">Starting price is required;</div>';
+        die();
+    } else {
+        if($_POST['startingPrice'] < 0) {
+            echo '<div class="alert alert-danger"role="alert">Staring price should be larger than 0;</div>';
+            die();
+        } else {
+            $startingPrice = $_POST['startingPrice'];
+        }
+    }
+
 
     //check reservePrice
-    if($_POST['reservePrice'] < $startingPrice) {
-        echo '<div class="alert alert-danger"role="alert">Reserve price should be larger than starting price</div>'; 
-        die();  
+    if (empty($_POST['reservePrice'])) {
+        $reservePrice = $_POST['startingPrice'];
     } else {
-        $reservePrice = $_POST['reservePrice'];
+        if($_POST['reservePrice'] < $startingPrice) {
+            echo '<div class="alert alert-danger"role="alert">Reserve price should be larger than starting price</div>';
+            die();   
+        } else {
+            $reservePrice = $_POST['reservePrice'];
+        }
     }
     //check endDate
     $currentDate = date('Y/m/d H:i');
@@ -36,7 +68,7 @@ if(isset($_POST['createAuction'])) {
     
     if($getEndDate < $currentDate) {
         echo '<div class="alert alert-danger"role="alert">End date should be larger than current time</div>';
-        die(); 
+        die();
     } else {
         $endDate = $getEndDate;
     }
@@ -47,17 +79,27 @@ if(isset($_POST['createAuction'])) {
 
     /* TODO #3: If everything looks good, make the appropriate call to insert
             data into the database. */
+
     $sql = "INSERT INTO Auctions(username,title,details,category,startingPrice,reservePrice,endDate) VALUES ('$username', '$title', '$details', '$category', '$startingPrice', '$reservePrice', timestamp '$w' )";
+
 
     if(mysqli_query($conn,$sql)) {
         // If all is successful, let user know.
-        echo '<div class="alert alert-success text-center"role="alert">Auction sucessfully created <a href="listing.php" class="alert-link">View your new listing.</a></div>';
-        "Location: index.php";
-        die();
+        //echo '<div class="alert alert-success text-center"role="alert">Auction sucessfully created <a href="mylisting.php" class="alert-link">View your new listing.</a></div>';
+        //"Location: index.php";
+        $fetch = " SELECT max(auctionID) FROM Auctions GROUP BY  '$username' " ;
+        $item = mysqli_fetch_row(mysqli_query($conn,$fetch));
+        $item_id = $item[0];
+        echo "<div class='alert alert-success text-center'role='alert'>Auction sucessfully created <a href='listing.php?item_id=$item_id' class='alert-link'>View my new listing.</a> </div>";
+        header('Location:mylistings.php');
     }  else {
         echo 'Error: ' .mysqli_error($conn);
-        echo"<a href='create_auction.php'>Unsucessfull</a>";
+        //echo"<a href='create_auction.php'>Unsucessfull</a>";
+        $_SESSION['msg'] = '<div class="alert alert-danger" role="alert">
+            Auction create unsucessful.
+        </div>';
     }   
+
 }
 
 
