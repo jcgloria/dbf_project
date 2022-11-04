bb5a84415c91ffedb32cfbba9da5fb57497b052a
adoganis <alexdoganis@usheron.com>
2022-11-04 15:21:56 +0000

    add recommendation functionality

--- auction/recommendations.php
+++ auction/recommendations.php
@@ -1,5 +1,15 @@
 <?php include_once("header.php")?>
 <?php require("utilities.php")?>
+<?php
+#Get the session of the user
+  session_start();
+  if (isset($_SESSION) && $_SESSION['logged_in']) {
+    $username = $_SESSION['username'];
+  } else {
+    echo '<p>You must login to view your recommendations</p>';
+    die();
+  }
+?>
 
 <div class="container">
 
@@ -7,16 +17,122 @@
 
 <?php
   // This page is for showing a buyer recommended items based on their bid 
-  // history. It will be pretty similar to browse.php, except there is no 
-  // search bar. This can be started after browse.php is working with a database.
-  // Feel free to extract out useful functions from browse.php and put them in
-  // the shared "utilities.php" where they can be shared by multiple files.
-  
-  
-  // TODO: Check user's credentials (cookie/session).
-  
-  // TODO: Perform a query to pull up auctions they might be interested in.
-  
-  // TODO: Loop through results and print them out as list items.
-  
+  // history.
+
+  // Get every user's bid history and count the auctions they have bid on in common with the current user
+  $sql_rank_similar_users = "SELECT DISTINCT u1.username, COUNT(*) AS rank
+      FROM Users u1, Bids b1, Bids b2
+      WHERE u1.username = b1.username AND b2.username = '".$_SESSION['username']."' AND b1.auctionId = b2.auctionId AND b1.username != b2.username
+      GROUP BY u1.username";
+
+  // Get all auctionIDs that the users from the resulting query have bid on that the current user has not bid on
+  // Sum the rankings for each auctionId and order by the sum
+  // Add auction details to each row if the endDate is after the current date
+  $sql_recommendations = "SELECT recoms.auctionId, recoms.title, recoms.details, recoms.endDate, recoms.category, recoms.rank, COUNT(b.bidID) as numBids,
+        case when count(b.bidId) > 0 then MAX(b.bidPrice)
+        else recoms.startingPrice
+        end as currentPrice
+      FROM (
+        SELECT a.auctionId, a.title, a.details, a.endDate, a.category, a.startingPrice, SUM(rank) AS rank 
+        FROM (".$sql_rank_similar_users.") AS similar_users, Auctions a
+        WHERE a.auctionId IN (
+            SELECT bw1.auctionId
+            FROM Bids bw1
+            WHERE bw1.username = similar_users.username
+        ) AND a.auctionId NOT IN (
+            SELECT bw2.auctionId
+            FROM Bids bw2
+            WHERE bw2.username = '".$_SESSION['username']."'
+        )
+        GROUP BY a.auctionId
+      ) as recoms
+      join Bids as b on recoms.auctionId = b.auctionId
+      WHERE recoms.endDate >= NOW()
+      group by recoms.auctionId
+      ORDER BY rank DESC";
+
+  $result = mysqli_query($conn, $sql_recommendations);
+  $rowcount = mysqli_num_rows($result);
+  $results_per_page = 10; // Number of results to display per page
+  $max_page = ceil($rowcount / $results_per_page);
 ?>
+
+<div class="container mt-5">
+
+  <?php if ($rowcount == 0) {
+    echo '<p class="text-center">No results found.</p>';
+    die();
+  }
+  ?>
+
+  <ul class="list-group">
+    <?php
+    while ($row = mysqli_fetch_array($result)) {
+      print_listing_li($row['auctionId'], $row['title'], $row['details'], $row['currentPrice'], $row['numBids'], new DateTime($row['endDate']));
+    }
+    ?>
+  </ul>
+
+
+  <!-- Pagination for results listings -->
+  <nav aria-label="Search results pages" class="mt-5">
+    <ul class="pagination justify-content-center">
+
+      <?php
+
+      // Copy any currently-set GET variables to the URL.
+      $querystring = "";
+      foreach ($_GET as $key => $value) {
+        if ($key != "page") {
+          $querystring .= "$key=$value&amp;";
+        }
+      }
+
+      $high_page_boost = max(3 - $curr_page, 0);
+      $low_page_boost = max(2 - ($max_page - $curr_page), 0);
+      $low_page = max(1, $curr_page - 2 - $low_page_boost);
+      $high_page = min($max_page, $curr_page + 2 + $high_page_boost);
+
+      if ($curr_page != 1) {
+        echo ('
+    <li class="page-item">
+      <a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page - 1) . '" aria-label="Previous">
+        <span aria-hidden="true"><i class="fa fa-arrow-left"></i></span>
+        <span class="sr-only">Previous</span>
+      </a>
+    </li>');
+      }
+
+      for ($i = $low_page; $i <= $high_page; $i++) {
+        if ($i == $curr_page) {
+          // Highlight the link
+          echo ('
+    <li class="page-item active">');
+        } else {
+          // Non-highlighted link
+          echo ('
+    <li class="page-item">');
+        }
+
+        // Do this in any case
+        echo ('
+      <a class="page-link" href="browse.php?' . $querystring . 'page=' . $i . '">' . $i . '</a>
+    </li>');
+      }
+
+      if ($curr_page != $max_page) {
+        echo ('
+    <li class="page-item">
+      <a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page + 1) . '" aria-label="Next">
+        <span aria-hidden="true"><i class="fa fa-arrow-right"></i></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </li>');
+      }
+      ?>
+
+    </ul>
+  </nav>
+
+
+</div>
